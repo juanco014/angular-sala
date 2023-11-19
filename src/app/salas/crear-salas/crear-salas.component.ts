a88544e57152a094c394f45ece4b60521c08a1da
Juan Arias <juan.arias06@uceva.edu.co>
2023-11-19 17:55:41 -0500

    Creacion del componente crear salas

---
+++ src/app/salas/crear-salas/crear-salas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crear-salas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crear-salas.component.html',
+  styleUrl: './crear-salas.component.css'
+})
+export class CrearSalasComponent {
+
+}
