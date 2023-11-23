38e16d35c6a219a0693f7190f101f18563f4fac7
juanco014 <135770213+juanco014@users.noreply.github.com>
2023-11-23 16:00:30 -0500

    arreglos del codigox2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes } from "@angular/router";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import {RouterModule, Routes} from "@angular/router";
-//import {ListarCursosComponent} from "./cursos/listar-cursos/listar-cursos.component";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-//import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
-import {ReactiveFormsModule} from "@angular/forms";
+
+
 
 const rutas: Routes =[
   //{path: '', redirectTo: 'listar-cursos', pathMatch: 'full'},
@@ -21,14 +19,12 @@ const rutas: Routes =[
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     //SweetAlert2Module.forRoot(),
     BrowserModule,
-    HttpClientModule,
     RouterModule.forRoot(rutas),
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
