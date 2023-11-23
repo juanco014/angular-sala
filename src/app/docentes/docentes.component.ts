01dcd6613a969dc7e3492ae473f283aa4dfc2940
Escandis <131204841+Escandis@users.noreply.github.com>
2023-11-22 23:17:02 -0500

    Creacion metodo docentes

---
+++ src/app/docentes/docentes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-docentes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './docentes.component.html',
+  styleUrl: './docentes.component.css'
+})
+export class DocentesComponent {
+
+}
