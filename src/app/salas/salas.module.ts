e2af8ea238a117ad7150e198eb689175727eeba4
juanco014 <135770213+juanco014@users.noreply.github.com>
2023-11-23 15:49:20 -0500

    arreglos del codigo

--- src/app/salas/salas.module.ts
+++ src/app/salas/salas.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListarSalasComponent } from './listar-salas/listar-salas.component';
+import { CrearSalaComponent } from './crear-sala/crear-sala.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarSalasComponent,
+    CrearSalaComponent
+
+  ],
+  exports:[
+    ListarSalasComponent
+  ],
   imports: [
     CommonModule
   ]
